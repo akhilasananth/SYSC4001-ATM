fcb30444e73a2f569db7b0a3bebe677cb4af3135
akhilasananth <akhilasananth@gmail.com>
2016-12-08 17:25:12 -0500

    DBeditor fixes

--- DBEditor.c
+++ DBEditor.c
@@ -6,12 +6,14 @@
 #include "messageTypes.h"
 #include "messageStructs.h"
 #include <string.h>
+#include <ctype.h>
 
-void checkForExit(char * userInput);
+void checkForExit(char* userInput);
+int checkIfNumbers(char* arr);
 
 //Variables
 int ServerEditorMsgqid;
-
+char* fundsInput;
 int main (void){
 	
 	printf("Starting DBEditor...\n Type \"X\" to exit\n");
@@ -22,34 +24,24 @@ int main (void){
 	}
 	while(1){
 	//Get account information
-	//********************************************************
 	my_message editorInput;
-	//char* fundsInput = "";
-	while(1){
-		
-		printf("Enter your 5 digit account number: ");
-		scanf("%s", editorInput.accountInfo.accountNum);
-		editorInput.accountInfo.accountNum[5] = '\0';
-		if(strcmp(editorInput.accountInfo.accountNum,"X")==0 || strcmp(editorInput.accountInfo.accountNum,"x")==0 ){
-			printf("Closing ATM\n");
-			exit(1);
-		}
-		
-		if(strlen(editorInput.accountInfo.accountNum) != 5){
-			perror("Invalid entry\n");
-			continue;
-		}
+	fundsInput = (char *)malloc(100);
+		while(1){
+			
+			printf("Enter your 5 digit account number: ");
+			scanf("%s", editorInput.accountInfo.accountNum);
+			editorInput.accountInfo.accountNum[5] = '\0';
+			if(strcmp(editorInput.accountInfo.accountNum,"X")==0 || strcmp(editorInput.accountInfo.accountNum,"x")==0 ){
+				printf("Closing ATM\n");
+				exit(1);
+			}
+			
+			
+			if(strlen(editorInput.accountInfo.accountNum) != 5 || checkIfNumbers(editorInput.accountInfo.accountNum)==0){
+				perror("Invalid entry\n");
+				continue;
+			}
 
-		printf("Enter your 3 digit PIN number: ");
-		scanf("%s", editorInput.accountInfo.pin);
-		editorInput.accountInfo.pin[3] = '\0';
-		if(strcmp(editorInput.accountInfo.pin,"X")==0 || strcmp(editorInput.accountInfo.pin,"x")==0 ){
-			printf("Closing ATM\n");
-			exit(1);
-		}
-
-		while(strlen(editorInput.accountInfo.pin) != 3){
-			perror("Invalid entry\n");
 			printf("Enter your 3 digit PIN number: ");
 			scanf("%s", editorInput.accountInfo.pin);
 			editorInput.accountInfo.pin[3] = '\0';
@@ -57,42 +49,62 @@ int main (void){
 				printf("Closing ATM\n");
 				exit(1);
 			}
-			
-		}
-		 
-		printf("Enter funds available: ");
-		scanf("%f", &editorInput.funds);
-		//sprintf(fundsInput,"%f",editorInput.funds);
-		//checkForExit(fundsInput);
 
-		while(editorInput.funds< 0){ //Checks for negative money
+			while(strlen(editorInput.accountInfo.pin) != 3 || checkIfNumbers(editorInput.accountInfo.pin)==0){
+				perror("Invalid entry\n");
+				printf("Enter your 3 digit PIN number: ");
+				scanf("%s", editorInput.accountInfo.pin);
+				editorInput.accountInfo.pin[3] = '\0';
+				if(strcmp(editorInput.accountInfo.pin,"X")==0 || strcmp(editorInput.accountInfo.pin,"x")==0 ){
+					printf("Closing ATM\n");
+					exit(1);
+				}
+				
+			}
+			 
 			printf("Enter funds available: ");
 			scanf("%f", &editorInput.funds);
-			//sprintf(fundsInput,"%f",editorInput.funds);
-			//checkForExit(fundsInput);
+			sprintf(fundsInput,"%f",editorInput.funds);
+			checkForExit(fundsInput);
+
+			while(editorInput.funds< 0 || !isdigit(editorInput.funds)){ //Checks for negative money
+				printf("Enter funds available: ");
+				scanf("%f", &editorInput.funds);
+				sprintf(fundsInput,"%f",editorInput.funds);
+				checkForExit(fundsInput);
+			}
+			break;
 		}
-		break;
-	}
-	//********************************************************
+		//********************************************************
 	
-	//Send DB Message to DB Server
-		editorInput.message_type = updateDB;
-		
-		int msgLength = sizeof(my_message) - sizeof(long);
-		if(msgsnd(ServerEditorMsgqid, &editorInput, msgLength,0) == -1){
-			perror("msgsnd: msgsnd failed\n");
-			exit(1);
-		}else{
-			printf("Success: Update Message sent to the Server\n\n");
-		}
+		//Send DB Message to DB Server
+			editorInput.message_type = updateDB;
+			
+			int msgLength = sizeof(my_message) - sizeof(long);
+			if(msgsnd(ServerEditorMsgqid, &editorInput, msgLength,0) == -1){
+				perror("msgsnd: msgsnd failed\n");
+				exit(1);
+			}
 	}
 	return 1;
 }
 
-void checkForExit(char * userInput){
+void checkForExit(char* userInput){
 	if(strcmp(userInput,"X")==0 || strcmp(userInput,"x")==0 ){
 		printf("Closing ATM\n");
 		exit(1);
 	}
 }
 
+int checkIfNumbers(char* arr){
+	int i;
+	int ret = 1;
+	for(i = 0; i< sizeof(arr)/sizeof(char); i++){
+		if(isdigit(arr[i])){
+			ret = 0;
+		}
+	}
+	
+	return ret;
+}
+		
